2f5a6c0902137607e2fb895ce735492b57ca59f9
Gabriel Regel <161236355+GabrielRegel@users.noreply.github.com>
2025-05-27 09:19:42 -0300

    Updated

--- cadastroDePessoas/src/cadastro/Main.java
+++ cadastroDePessoas/src/cadastro/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
 		do {
 			System.out.println("\n===== MENU =====");
-			System.out.println("1 -Cadastrar pessoa");
+			System.out.println("1 - Cadastrar pessoa");
 			System.out.println("2 - Listar pessoas");
 			System.out.println("0 - Sair");
 			System.out.println("Escolha uma opção: ");
@@ -21,7 +21,7 @@ public class Main {
 			
 			switch (opcao) {
 			case 1:
-				System.out.println("Nome: ");
+				System.out.println("Nome:  ");
 				String nome = scanner.nextLine();
 				scanner.nextLine();
 				
@@ -30,10 +30,18 @@ public class Main {
 				scanner.nextLine();
 				
 				System.out.println("Email: ");
-				int email = scanner.nextInt();
+				String email = scanner.nextLine();
 				scanner.nextLine();
 				
-				pessoas[contador] = new Pessoa(nome, idade, email);
+				System.out.println("Telefone: ");
+				double telefone = scanner.nextDouble();
+				scanner.nextLine();
+				
+				System.out.println("CPF: ");
+				String cpf = scanner.nextLine();
+				scanner.nextLine();
+				
+				pessoas[contador] = new Pessoa(nome, idade, email, cpf, telefone);
 				contador++;
 				System.out.println("Pessoa cadastrada com sucesso!");
 				break;
@@ -43,13 +51,21 @@ public class Main {
 					System.out.println("Nenhuma pessoa cadstrada.");
 				}else {
 					for(int i=0; i<contador;i++) {
-						
+						pessoas[i].exibirDados();
 					}
 				}
-			break;
+				break;
+			
+			case 0:
+				System.out.println("Saindo...");
+				break;
 				
+			default:
+				System.out.println("Opção inválida.");
 			}
-		}
+		} while  (opcao != 0);
+			scanner.close();
+		
 	
 	}
 
--- cadastroDePessoas/src/cadastro/Pessoa.java
+++ cadastroDePessoas/src/cadastro/Pessoa.java
@@ -1,23 +1,44 @@
 package cadastro;
 
+import java.text.DecimalFormat;
+
 public class Pessoa {
 
 	private String nome;
 	private int idade;
 	private String email;
+	private double telefone;
+	private String cpf;
+
 	
 	
-	public Pessoa(String nome, int idade, String email) {
+	public Pessoa(String nome, int idade, String email, String cpf, double telefone) {
 		this.nome = nome;
 		this.idade = idade;
 		this.email = email;
+		this.telefone = telefone;
+		this.cpf = cpf;
+		
 	}
 	
+	
+	
+	public Pessoa() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+
 	public void exibirDados() {
+		System.out.println("\n\n");
 		System.out.println("----- Dados da Pessoa -----");
-		System.out.println("Nome: " +  nome);
-		System.out.println("Idade: " + idade);
-		System.out.println("Email: " + email);
+		System.out.println("Nome:     " + nome);
+		System.out.println("Idade:    " + idade);
+		System.out.println("Email:    " + email);
+		System.out.println("Telefone: " + telefone);
+		System.out.println("CPF:      " + cpf);
+		System.out.println("\n\n");
 
 	}
 	
